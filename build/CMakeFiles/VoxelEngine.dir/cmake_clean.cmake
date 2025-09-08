a3faefe6cee854e575f3a2fc36959672793bd216
Nick Izbrodin <nizbrodin@gmail.com>
2025-09-08 12:09:05 +0100

    Initial commit

---
+++ build/CMakeFiles/VoxelEngine.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VoxelEngine.dir/main.cpp.obj"
+  "CMakeFiles/VoxelEngine.dir/main.cpp.obj.d"
+  "VoxelEngine.exe"
+  "VoxelEngine.exe.manifest"
+  "VoxelEngine.pdb"
+  "libVoxelEngine.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VoxelEngine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
